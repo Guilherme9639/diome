e4c5ac19885139b8fa48a35ac9224d9faadfeeb1
Guilherme de Almeida <cardozoalmeidaguilherme@gmail.com>
2023-10-31 19:31:01 -0300

    Entendendo as variaveis e constantes

--- Exercicios.py
+++ Exercicios.py
@@ -1,1 +1,10 @@
 age, name = (23, "Guilherme")
+##Boas Praticas##
+#Escolher nomes sugestivos#
+#O padrão de nomes deve ser sanke case
+#Nomes de constantes todo em maiusculo.#
+nome = "Guilherme"
+idade = 23
+limite_saque_diario = 1000
+BRAZILIAN_STATES = ("SP", "RJ", "SC")
+print(nome, idade, BRAZILIAN_STATES)
