69c31f994a990cba57fcd68ee13eb58690a5427f
Guilherme de Almeida <cardozoalmeidaguilherme@gmail.com>
2023-10-31 20:09:23 -0300

    Funções de entrada e saida

--- Exercicios.py
+++ Exercicios.py
@@ -5,6 +5,14 @@ age, name = (23, "Guilherme")
 #Nomes de constantes todo em maiusculo.#
 nome = "Guilherme"
 idade = 23
+sobrenome = "Almeida"
 limite_saque_diario = 1000
 BRAZILIAN_STATES = ("SP", "RJ", "SC")
 print(nome, idade, BRAZILIAN_STATES)
+
+#Funções de entrada e saida
+nome = input("Informe o seu nome:")
+idade = input("Informe a sua idade")
+print(nome, sobrenome)
+print(nome, sobrenome, end="... \n")
+print(nome, idade, sep="#")
