4cd50feda66d86eb3b159638bf1651390a34ece3
Guilherme de Almeida <cardozoalmeidaguilherme@gmail.com>
2023-11-01 16:05:33 -0300

    Indo acessar do notebook

--- Exercicios.py
+++ Exercicios.py
@@ -11,13 +11,34 @@ BRAZILIAN_STATES = ("SP", "RJ", "SC")
 print(nome, idade, BRAZILIAN_STATES)
 
 #Funções de entrada e saida
-nome = input("Informe o seu nome:")
-idade = input("Informe a sua idade")
-print(nome, sobrenome)
-print(nome, sobrenome, end="... \n")
-print(nome, idade, sep="#")
-print(float("a"))
+#nome = input("Informe o seu nome:")
+#idade = input("Informe a sua idade")
+#print(nome, sobrenome)
+#print(nome, sobrenome, end="... \n")
+#print(nome, idade, sep="#")
+#print(float("a"))
 
-list(range(10,1,-1))
+#list(range(10,1,-1))
 
-print(list)
+#print(list)
+
+##Tipos de Operadores com Python##
+#Operadores de comparação
+saldo = 200
+saque = 200
+
+#print(saldo == saque)
+#print(saldo > saque)
+#print(saldo >= saque)
+#print(saldo < saque)
+#print(saldo <= saque)
+
+#Operadores de Atribuição
+saldo = 500
+saldo += 200
+saldo -= 200
+saldo *= 200
+saldo //= 200
+print(saldo)
+
+#Operadores Logicos
