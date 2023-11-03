f21989421d6651fa6138053f09685e370f96e8bc
Guilherme9639 <83843448+Guilherme9639@users.noreply.github.com>
2023-11-03 18:48:45 -0300

    Create exercicios_condicionais.py

---
+++ exercicios_condicionais.py
@@ -0,0 +1,25 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque:"))
+if saldo >= saque:
+    print("Saque realizado com sucesso")
+if saldo < saque:
+    print("Seu saque não pode ser efetuado")
+
+
+
+if saldo >= saque:
+    print("saque realizado com sucesso")
+else:
+    print("Saldo Insuficiente")  
+
+
+opcao = int(input("Informe uma opção: [1] Sacar \n[2] Extrato:"))
+
+if opcao == 1:
+    valor = float(input("Informe a quantia para o saque: "))
+
+elif opcao == 2:
+    print("Exibindo o extrato...")
+
+else:
+    sys.exit("Opção Inválida")
