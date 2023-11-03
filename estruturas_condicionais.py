80fa2645ea108229bf54e8e85cc64101691d3bad
guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2023-11-03 18:45:22 -0300

    nova etapa do curso, novo arquivo.

---
+++ estruturas_condicionais.py
@@ -0,0 +1,10 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque:"))
+if saldo >= saque:
+    print("Saque realizado com sucesso")
+if saldo < saque:
+    print("Seu saque não pode ser efetuado")
+
+
+
+  
