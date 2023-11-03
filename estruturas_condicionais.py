267b6da35b52d936063484b36d7f51df5c513d2e
guilherme9639 <cardozoalmeidaguilherme@gmail.com>
2023-11-03 18:46:56 -0300

    Novas aulas, novo arquivo.

--- estruturas_condicionais.py
+++ estruturas_condicionais.py
@@ -7,4 +7,19 @@ if saldo < saque:
 
 
 
-  
+if saldo >= saque:
+    print("saque realizado com sucesso")
+else:
+    print("Saldo Insuficiente")  
+
+
+opcao = int(input("Informe uma opção: [1] Sacar \n[2] Extrato:"))
+
+if opcao == 1:
+    valor = float(input("Informe a quantia para o saque: "))
+
+elif opcao == 2:
+    print("Exibindo o extrato...")
+
+else:
+    sys.exit("Opção Inválida")
