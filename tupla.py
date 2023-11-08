8865d4ea5e916a0926d20ce2c4945b914d002eb4
Guilherme de Almeida <cardozoalmeidaguilherme@gmail.com>
2023-11-08 07:17:43 -0300

    tupla

---
+++ tupla.py
@@ -0,0 +1,18 @@
+# diferente da lista, a tupla não muda após sua criação
+
+frutas = ("laranja", "pera", "uva",) #utilizar sempre uma virgula no final da tupla
+
+letras = tuple("python")
+
+print(frutas[0])
+print(frutas[2])
+
+matriz = (               
+    (1, "a", 2),
+    ("b", 3, 4),
+)
+
+frutas.count() #metodos
+frutas.idex()
+frutas.len()
+
