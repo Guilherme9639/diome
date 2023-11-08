103d25b8399dfe89978e6ea778867d0ea5aa7aa0
Guilherme de Almeida <cardozoalmeidaguilherme@gmail.com>
2023-11-07 23:13:12 -0300

    listas com python

--- listas.py
+++ listas.py
@@ -8,3 +8,28 @@ lista.clear() #significa limpar aquela lista
 
 print(lista)
 
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 42000]
+print(carro)
+
+matriz = [
+    [1, "a", 2]
+    [2, "a", 1]
+    ["a", 1, 2]
+]
+
+print(matriz[0])
+
+print(matriz[0][0])
+
+print(matriz[0][-1])
+
+print(matriz[-1][-1])
